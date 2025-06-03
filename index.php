07848e559cfbef7d858de95a86b9b6c7f5300136
Serhii Veretko <l1stlessofficial@gmail.com>
2025-06-03 13:09:05 +0300

    added r

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $length = 10;
 $width  = 5;
 $area   = $length * $width;
 echo "Площа прямокутника ($length × $width) = $area<br>";
-
+//r
 // ——— 2. Довжина рядка ———
 $text = "Привіт, PHP!";
 $chars = mb_strlen($text, 'UTF-8'); // рахуй UTF-8-символи
